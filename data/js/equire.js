3286287717a68f09663d4fdc07258bdef6587de6
GamingOnInsulin <gamingoninsulin@gmail.com>
2024-12-06 16:28:00 +0100

    draft 2 for mobile

--- data/js/equire.js
+++ data/js/equire.js
@@ -1,10 +1,10 @@
 enquire.register('screen and (max-width: 768px)', {
   match: () => {
-    const mainContent = document.getElementById('main-content');
+    const mainContent = document.getElementById('main');
     mainContent.classList.add('simplified-view');
   },
   unmatch: () => {
-    const mainContent = document.getElementById('main-content');
+    const mainContent = document.getElementById('main');
     mainContent.classList.remove('simplified-view');
   },
 });
