058fc0e1fdbd16a31d2a248e11f8fe6a9dbdcf0d
GamingOnInsulin <gamingoninsulin@gmail.com>
2024-12-07 20:46:34 +0100

    fixxed compatibility mobile i think well lets try

--- data/js/equire.js
+++ data/js/equire.js
@@ -1,4 +1,4 @@
-enquire.register('screen and (max-width: 768px)', {
+enquire.register('screen and (max-width: 720px)', {
   match: () => {
     const mainContent = document.getElementById('main');
     mainContent.classList.add('simplified-view');
